65dfd571fa807f17ed3caa4e9f1e5936b7657f06
Sandeep Mehta <112331347+sandeepkrmehta@users.noreply.github.com>
2024-11-08 12:27:12 +0530

    Update
    backend\package-lock.json
    client\package-lock.json
    backend\app.js

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,5 @@
-import { configDotenv } from 'dotenv';
-configDotenv();
+import dotenv from 'dotenv';
+dotenv.config();
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import morgan from 'morgan';
@@ -13,27 +13,30 @@ import errorMiddleware from './middleware/error.middleware.js';
 
 const app = express();
 
-// middleware
+// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(morgan('dev'));
 app.use(cors({ origin: [process.env.CLIENT_URL], credentials: true }));
 
-
+// Routes
 app.use('/api/v1/user', userRoutes); 
 app.use('/api/v1/courses', courseRoutes); 
 app.use('/api/v1/payments', paymentRoutes);
 app.use('/api/v1/', miscellaneousRoutes);
- 
 
+// 404 Handler
 app.all('*', (req, res) => {
-    res.status(404).send('OOPS!! 404 page not found');
-})
+  res.status(404).json({
+    message: `OOPS!! ${req.method} ${req.originalUrl} not found`,
+  });
+});
 
+// Error Handling Middleware
 app.use(errorMiddleware);
 
-// db init
+// Database Initialization
 connectToDb();
 
 export default app;
