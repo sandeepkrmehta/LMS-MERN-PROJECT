7910ff7552f329be92623fd44a5adc4cdbb9a428
Sandeep Mehta <112331347+sandeepkrmehta@users.noreply.github.com>
2024-11-08 13:35:20 +0530

    Update
    client\vite.config.js
    client\package.json

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     hmr: {
       overlay: false,
     },
+    port: process.env.PORT || 3000, // Use environment variable PORT or 3000 as fallback
+    host: '0.0.0.0', // Necessary for external connections
   },
-})
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
+      },
+    },
+  },
+});
