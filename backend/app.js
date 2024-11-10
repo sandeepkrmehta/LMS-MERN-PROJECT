9879bc7dca2bf8ae326d004cf2fa04ecee01becc
Sandeep Mehta <112331347+sandeepkrmehta@users.noreply.github.com>
2024-11-11 01:09:18 +0530

    update and remove server.js
    backend\controllers\payment.controller.js
    backend\app.js
    client\src\main.jsx
    backend\package.json

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,12 @@ import miscellaneousRoutes from './routes/miscellaneous.routes.js';
 import express from 'express';
 import connectToDb from './config/db.config.js';
 import errorMiddleware from './middleware/error.middleware.js';
+import { v2 as cloudinary } from 'cloudinary';
+import Razorpay from "razorpay"; 
 
+const PORT = process.env.PORT;
+
+// Initialize Express app
 const app = express();
 
 // Middleware
@@ -18,13 +23,27 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(morgan('dev'));
-app.use(cors({ origin: [process.env.CLIENT_URL], credentials: true }));
+
+// CORS setup
+app.use(cors({
+  origin: [process.env.CLIENT_URL],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 
 // Routes
 app.use('/api/v1/user', userRoutes); 
 app.use('/api/v1/courses', courseRoutes); 
 app.use('/api/v1/payments', paymentRoutes);
 app.use('/api/v1/', miscellaneousRoutes);
+
+// Error Handling Middleware
+app.use(errorMiddleware);
+
+// Home Route
+app.get('/', (req, res) => {
+  res.send('Welcome to the Home Page!');
+});
 
 // 404 Handler
 app.all('*', (req, res) => {
@@ -33,10 +52,25 @@ app.all('*', (req, res) => {
   });
 });
 
-// Error Handling Middleware
-app.use(errorMiddleware);
+// Cloudinary configuration
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET 
+});
+
+// Razorpay configuration  
+export const razorpay = new Razorpay({
+  key_id: process.env.RAZORPAY_KEY_ID,
+  key_secret: process.env.RAZORPAY_SECRET
+})
 
 // Database Initialization
 connectToDb();
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server started at http://localhost:${PORT}`);
+});
 
 export default app;
